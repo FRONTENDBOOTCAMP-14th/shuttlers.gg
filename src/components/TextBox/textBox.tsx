1ef18cd496c9caca7043676722cda10c975a0974
Dowap2 <dowapdowari@gmail.com>
2025-10-13 10:12:33 +0900

    [repactor] 컨벤션 수정

--- src/components/TextBox/textBox.tsx
+++ src/components/TextBox/textBox.tsx
@@ -1,6 +1,6 @@
 import clsx from 'clsx';
 import React from 'react';
-import * as style from './textBox.css.js';
+import * as style from './TextBox.css.js';
 
 export type TextBoxProps = {
   text: string;
