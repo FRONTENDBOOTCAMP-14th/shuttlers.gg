75d35d48a7dcff8b1ef675db35062e42f47cc7e4
Dowap2 <dowapdowari@gmail.com>
2025-10-14 11:48:20 +0900

    [feat] UserResultCard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import { UserResultCard } from '../components/UserResultCard/UserResultCard';
-
 export default function HomePage() {
-  return (
-    <>
-      <UserResultCard></UserResultCard>
-    </>
-  );
+  return <></>;
 }
