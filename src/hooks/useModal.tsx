9280a959beec447bc90e57f26273671916738f56
rhocci <4tres1@gmail.com>
2025-10-15 11:16:30 +0900

    [docs] useModal 사용법 주석 추가

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -1,5 +1,25 @@
 import { useState } from 'react';
 
+/**
+ * 모달 열림/닫힘 상태를 제어하는 커스텀 훅입니다.
+ *
+ * @example
+ * // 기본 사용법
+ * const modal = useModal();
+ * // 열기
+ * modal.open();
+ * // 닫기
+ * modal.close();
+ * // 상태값
+ * modal.isOpen // true | false
+ *
+ * @param {boolean} [initialState=false] - 모달 열림 상태
+ * @returns {{
+ *   isOpen: boolean;
+ *   open: () => void;
+ *   close: () => void;
+ * }}
+ */
 export default function useModal(initialState = false) {
   const [isOpen, setIsOpen] = useState(initialState);
 
